6fef25d6507087a8be0b7c0af9f35d0ccc3bb052
Bradley Marques <brad@next45.co>
2017-09-28 16:12:12 +0200

    Adds Counter component

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+console.log('main.js loaded through bundle.js')
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import Counter from './Counter';
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOMContentLoaded');
+});
