d46747bc209acdc025bea75595bedc543b294536
Bradley Marques <brad@next45.co>
2017-09-28 17:51:38 +0200

    Changes entry point for app to index.js and adds redux and react-redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import { createStore } from 'redux'
 import catApp from './reducers'
 
-let store = createStore(catApp) // Optional second argument to set initial state (from server)
+let store = createStore(
+  catApp, /* initial_state_from_server, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+
+// TESTING:
+
+//import { addCat, renameCat } from './actions'
+
+//console.log()
